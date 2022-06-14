6e11db6b6e7a53337a4da311fa8342f67e4e3fa9
noradroid <75635291+noradroid@users.noreply.github.com>
2022-06-14 11:55:59 +0800

    Import hero details, hero services and mock hero data

---
+++ src/app/heroes/hero-list/hero-list.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { MessageService } from 'src/app/message.service';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-hero-list',
+  templateUrl: './hero-list.component.html',
+  styleUrls: ['./hero-list.component.css'],
+})
+export class HeroListComponent implements OnInit {
+  heroes: Hero[] = [];
+  selectedHero?: Hero;
+
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+  getHeroes() {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
+}
